968b8e99dda125fc19726e1b4643e657d1478a6e
harristheharris <anthonyharris7777@gmail.com>
2024-05-06 23:07:38 -0400

    getting my bearings and figuring it out

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
+const sequelize = require('sequelize');
 require('dotenv').config();
-
-const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
