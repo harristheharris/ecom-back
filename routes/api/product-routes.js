5a208a516ebc58fe739b6d1a3b0d365263dec4ef
harristheharris <anthonyharris7777@gmail.com>
2024-05-08 20:09:42 -0400

    idk

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,9 +21,18 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  try {
+    const productData = await Product.findByPk(req.param.id, {
+      include: [{}]
+    })
+  } catch (err) {
+
+  }
+
 });
 
 // create new product
