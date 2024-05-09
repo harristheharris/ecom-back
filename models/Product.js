45599b81d2da93a8e76560a93a0893de12ee41ad
harristheharris <anthonyharris7777@gmail.com>
2024-05-09 19:42:36 -0400

    finished models and reltionships

--- models/Product.js
+++ models/Product.js
@@ -14,9 +14,36 @@ Product.init(
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-
     },
-    
+    product_name: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    price:{
+      types: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      types: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
